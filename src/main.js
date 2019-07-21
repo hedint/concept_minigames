38ef74840fd36157c9726e98475ceadf13cddc92
v.volodin <v.volodin@corp.mail.ru>
2019-07-21 16:43:24 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
-import App from "./App.vue";
+import HelloWorld from "./components/HelloWorld";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
+const plugin = {
+  install() {
+    Vue.component("HelloWorld", HelloWorld);
+  }
+};
+Vue.use(plugin);
+console.log("INITED");
+export default plugin;
